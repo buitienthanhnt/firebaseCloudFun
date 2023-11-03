9686677fa6b96493a8c0a1b51e15580c51984f58
thanhnt <buitienthanhnt@gmail.com>
2023-11-03 15:29:51 +0700

    firebase cloud function push mesage upcode.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,7 @@
 // const { onRequest } = require("firebase-functions/v2/https");
 // const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
-const {faker} = require("@faker-js/faker");
+const { faker } = require("@faker-js/faker");
 const admin = require("firebase-admin");
 // const serviceAccount = require("./service_account.json");
 
@@ -33,9 +33,9 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-interface product{
-    name: string,
-    price: Number
+interface product {
+  name: string,
+  price: Number
 }
 // Initialize products array
 var products: product[] = [];
@@ -57,7 +57,7 @@ exports.listProducts = functions.https.onCall((data: any, context: any) => {
   return products;
 });
 
-interface UserItem{
+interface UserItem {
   id: Number,
   name: string,
   key: string,
@@ -67,16 +67,16 @@ exports.someMethod = functions.https.onCall((data: any, context: any) => {
   var listUser: UserItem[] = [];
   var db = admin.firestore();
   return db.collection("listUser").get().then((snapshot: any) => {
-      snapshot.forEach((doc: any) => {
-          var newelement = {
-              key: doc.id,
-              name: doc.data().name,
-              id: doc.data().id,
-          }
-          listUser.push(newelement);
-      });
-      // console.log('_____+',stuff);
-      return listUser;
+    snapshot.forEach((doc: any) => {
+      var newelement = {
+        key: doc.id,
+        name: doc.data().name,
+        id: doc.data().id,
+      }
+      listUser.push(newelement);
+    });
+    // console.log('_____+',stuff);
+    return listUser;
   });
 });
 
@@ -84,14 +84,14 @@ exports.someMethod = functions.https.onCall((data: any, context: any) => {
 exports.addMessage = functions.https.onCall((data: any, context: any) => {
   return admin.database().ref("/users")
     .push({
-        id: Math.floor(Math.random() * 100),
-        name: "user " + Math.floor(Math.random() * 100),
-        email: `user${Math.floor(Math.random() * 100)}@gmail.com`,
+      id: Math.floor(Math.random() * 100),
+      name: "user " + Math.floor(Math.random() * 100),
+      email: `user${Math.floor(Math.random() * 100)}@gmail.com`,
     })
     .then(() => {
-        console.log("Add new user");
-        // Returning the sanitized message to the client.
-        return { text: "ppp" };
+      console.log("Add new user");
+      // Returning the sanitized message to the client.
+      return { text: "ppp" };
     });
 });
 
@@ -129,15 +129,76 @@ exports.writeDB = functions.database
 
 exports.deleteListen = functions.database
   .ref("users")
-  .onDelete((snap : any, context: any) => {
+  .onDelete((snap: any, context: any) => {
     console.log("123");
   });
 
+// push message: https://engineering.monstar-lab.com/en/post/2021/02/09/Use-Firebase-Cloudfunctions-To-Send-Push-Notifications/
+// https://firebase.google.com/codelabs/firebase-cloud-functions?hl=vi#0
+//http request method
+exports.sendHttpPushNotification = functions.https.onRequest((req: any, res: any) => {
+  // const userId = req.body.userId; //get params like this
+  // const FCMToken = admin.database().ref(`/FCMTokens/${userId}`).once('value');
+  const FCMToken = "fRaRyvcESDS3hN_fTLRPcT:APA91bF0XBXrAHQUMSp_QyDmBl3Xlh0OZUz7nw8FV0x5IKH2j88WCP5muRI_8C5E03VxdoFGRVto60YKn7J6o4BfK4LeidK4GMZZIRiSRQX1M8g1btbxrienvxi3l_u3Eg7WHOOCfS0R";
+  const payload = {
+    token: FCMToken,
+    notification: {
+      title: 'req',
+      body: "message conten"
+    },
+    data: {
+      id: "123",
+      link: ""
+    }
+  };
+
+  // admin.messaging().sendToDevice(FCMToken, payload).then((response: any) =>{
+  //   console.log('====================================');
+  //   console.log(response);
+  //   console.log('====================================');
+  // });
+
+  admin.messaging().send(payload).then((response: any) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+    return { success: true };
+  }).catch((error: Error) => {
+    return { error: error.message };
+  });
+
+  return { error: "pôppopopopopo" };
+});
+
+//listener method
+exports.sendListenerPushNotification = functions.database.ref('/sendMessage/{userId}/').onWrite((data: any, context: any) => {
+  const userId = context.params.userId; //get params like this
+  const FCMToken = admin.database().ref(`/FCMTokens/${userId}`).once('value');
+
+  const payload = {
+    token: FCMToken,
+    notification: {
+      title: 'cloud function demo',
+      body: "message conten"
+    },
+    data: {
+      body: {},
+    }
+  };
+
+  admin.messaging().send(payload).then((response: any) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+    return { success: true };
+  }).catch((error: Error) => {
+    return { error: error.message };
+  });
+});
 
 
 
 
-// firebase.config 
+
+// firebase.config
 // {
 //   "functions": [
 //     {
